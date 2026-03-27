79922e53390aa24c0376e740d3686ac1e6d46ac1
0neAnd0nlyKen <kendrickraphael@gmail.com>
2026-03-28 04:42:49 +0800

    fe products lists component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,10 +2,12 @@
 import MainFooter from './components/layout/MainFooter.vue'
 import MainNavbar from './components/layout/MainNavbar.vue'
 import HeroSection from './components/sections/HeroSection.vue'
+import ProductsSection from './components/sections/ProductsSection.vue'
 </script>
 
 <template>
   <MainNavbar />
+  <ProductsSection />
   <HeroSection />
   <MainFooter />
 </template>
